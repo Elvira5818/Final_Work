3f5f74ddb7d93bb562fff235a37388b559d05d73
Elvira <elvira5818@yandex.ru>
2023-12-07 17:05:49 +0500

    добавили метод, который проверяет наличие знаков в каждом элементе массива

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 string [] arrayUser = new string[7] {"123", "qwerty", "dog" , "free1" , "5818q" , "cat" , ":-)"};
 string [] arraynew = new string [arrayUser.Length];
 
+CreateArrayThreeEl(arrayUser, arraynew);
+
+void CreateArrayThreeEl (string [] arrayUser, string [] arraynew)
+{
+    int count = 0;
+    for (int i = 0; i < arrayUser.Length; i++)
+    {
+        if (arrayUser[i].Length <= 3 )
+        {
+            arraynew[count] = arrayUser[i];
+            count++;
+        }
+    }
+
+}
